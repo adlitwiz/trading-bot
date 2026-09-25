5055e3a2b360bc752db90bf08072e95b1600588a
adlitwiz <adlivilla1@gmail.com>
2026-09-25 08:41:51 +0700

    Update BASE_URL to new Bybit API endpoint

--- bybit_fetch.py
+++ bybit_fetch.py
@@ -10,7 +10,7 @@ import time
 import requests
 import pandas as pd
 
-BASE_URL = "https://api.bybit.com"
+BASE_URL = "https://www.bybit-global.com/id-ID/"
 
 
 def get_all_perp_symbols(quote: str = "USDT") -> list:
